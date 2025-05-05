76f5ae469b702b6c6ae2557d81616569eefdb4e3
shehabhany4 <shehabhany4@outlook.com>
2025-05-05 18:41:13 +0300

    fin

--- j.js
+++ j.js
@@ -7,11 +7,5 @@ bars.onclick = function () {
         menu.style.display = 'none';
     } else {
         menu.style.display = 'flex';
-        menu.style.justifyContent = 'space-evenly';
-        menu.style.alignItems = 'center';
-        menu.style.background = 'rgba(46, 46, 46, 0.886)';
-        menu.style.flexDirection = 'column';
-        menu.style.width = '100%';
-        menu.style.height = '100vh';
     }
 }
