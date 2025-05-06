b8831d69407e24f098c24bbcf047cbd54ce1cd25
shehabhany4 <shehabhany4@outlook.com>
2025-05-06 18:50:47 +0300

    er9

--- j.js
+++ j.js
@@ -5,6 +5,11 @@ let c2 = document.querySelector('.c2');
 let ccm = document.getElementById('ccm');
 let bnm = document.getElementById('bnm');
 let one = document.querySelector('.one');
+let ho = document.getElementById('ho');
+let si = document.getElementById('si');
+let pr = document.getElementById('pr');
+let pi = document.getElementById('pi');
+let cwm = document.getElementById('cwm');
 
 bars.onclick = function () {
     if (menu.style.display === 'flex') {
@@ -13,7 +18,7 @@ bars.onclick = function () {
         c2.style.display = 'flex';
         ccm.style.display = 'flex';
         bnm.style.display = 'none';
-        one.style.display = 'flex'
+        one.style.display = 'flex';
     } else {
         menu.style.display = 'flex';
         c1.style.display = 'none';
@@ -22,4 +27,36 @@ bars.onclick = function () {
         bnm.style.display = 'block';
         one.style.display = 'none'
     }
+}
+ho.onclick = function () {
+    menu.style.display = 'none';
+    c1.style.display = 'block';
+    c2.style.display = 'flex';
+    ccm.style.display = 'flex';
+    bnm.style.display = 'none';
+    one.style.display = 'flex';
+}
+si.onclick = function () {
+    menu.style.display = 'none';
+    c1.style.display = 'none';
+    c2.style.display = 'none';
+    ccm.style.display = 'none';
+    bnm.style.display = 'block';
+    one.style.display = 'flex';
+}
+pr.onclick = function () {
+    menu.style.display = 'none';
+    c1.style.display = 'block';
+    c2.style.display = 'none';
+    ccm.style.display = 'none';
+    bnm.style.display = 'block';
+    one.style.display = 'none';
+}
+pi.onclick = function () {
+    menu.style.display = 'none';
+    c1.style.display = 'none';
+    c2.style.display = 'flex';
+    ccm.style.display = 'none';
+    bnm.style.display = 'block';
+    one.style.display = 'none';
 }
